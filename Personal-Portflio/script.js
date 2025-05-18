23402c5da3c043ee1f41c57384da0a4058bb46af
TaylorCTech <ctaylor.aspiring@gmail.com>
2025-05-17 22:48:21 -0500

    Delete Personal-Portflio directory

--- Personal-Portflio/script.js
+++
@@ -1,8 +0,0 @@
-let coursesBox = document.querySelector('.courses__box');
-let skillsBox = document.querySelector('.skills__box');
-
-let coursesBtn = document.querySelector('.coursesBtn');
-let skillsBtn = document.querySelector('.skillsBtn');
-
-let skillsTest = skillsBox.classlist.contains('hide');
-let coursesTest = skillsBox.classlist.contains('hide');
