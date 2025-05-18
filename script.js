f9695cc585e416885404c724cff7ac97f04fee04
TaylorCTech <ctaylor.aspiring@gmail.com>
2025-05-17 22:49:46 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+let coursesBox = document.querySelector('.courses__box');
+let skillsBox = document.querySelector('.skills__box');
+
+let coursesBtn = document.querySelector('.coursesBtn');
+let skillsBtn = document.querySelector('.skillsBtn');
+
+let skillsTest = skillsBox.classlist.contains('hide');
+let coursesTest = skillsBox.classlist.contains('hide');
