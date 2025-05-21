b8f8d6460882a8a270b492c11311ffe4669ed3bb
TaylorCTech <ctaylor.aspiring@gmail.com>
2025-05-21 14:11:02 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
-let coursesBox = document.querySelector('.courses__box');
-let skillsBox = document.querySelector('.skills__box');
+const skillsBtn = document.querySelector(".skillsBtn");
+const skillsBox = document.querySelector(".skills__box");
+const coursesBtn = document.querySelector(".coursesBtn");
+const coursesBox = document.querySelector(".courses__box");
 
-let coursesBtn = document.querySelector('.coursesBtn');
-let skillsBtn = document.querySelector('.skillsBtn');
+function skillsSection() {
+  skillsBox.classList.remove("hide");
+  coursesBox.classList.add("hide");
+  skillsBtn.classList.remove("inactive"); 
+  coursesBtn.classList.remove("active");
+  skillsBtn.classList.add("active"); 
+  coursesBtn.classList.add("inactive");
+}
 
-let skillsTest = skillsBox.classlist.contains('hide');
-let coursesTest = skillsBox.classlist.contains('hide');
+function coursesSection() {
+  coursesBox.classList.remove("hide");
+  skillsBox.classList.add("hide");
+  coursesBtn.classList.remove("inactive");
+  skillsBtn.classList.remove("active");
+  coursesBtn.classList.add("active");
+  skillsBtn.classList.add("inactive");
+}
